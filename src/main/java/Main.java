b9527a2c009f6730fa3e1874a8a7c9da9e4be7dc
Chandana Srinivasa <csriniva@usc.edu>
2015-06-25 11:09:24 -0700

    Added the files

---
+++ src/main/java/Main.java
@@ -0,0 +1,183 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+public class Main extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String jsonResponse="{\"query\":";
+    
+    String queryString = request.getParameter("url");
+    if (queryString == null) {
+      response.setStatus(HttpServletResponse.SC_OK);
+      jsonResponse+="\"\"}";
+      response.getWriter().append(jsonResponse);
+      response.getWriter().flush();
+      
+      return;
+    }
+    
+    Document doc = Jsoup.connect(queryString).userAgent("Mozilla").get();
+    
+    String frequentWords[] = {"and","at","be","but","by","if","into","it","no","not","of","or","such","an","the","a","their","then","there","these","this","to","was","will","with","so","also","that","they","therefore","for","much","more","hence","is","are","why","what","how","as","on","in","-","&"," "};
+    
+    boolean titleFound = false;
+    String titleRequired = "";
+    
+    String title = doc.title();
+    
+    if (!title.isEmpty())
+    { 
+      titleRequired = title;
+      titleFound = true;
+    }
+    else 
+    {
+      titleRequired = "NOT_FOUND";
+      titleFound = false;
+    }
+    
+    Elements metaLinksForDescription = doc.select("meta[name=description]");
+    boolean metaDescriptionFound = false;
+    List<String> descriptionList = new ArrayList<String>();
+    
+    String metaTagDescriptionContent = "", metaDescriptionRequired = "";
+    
+    if (!metaLinksForDescription.isEmpty())
+    { 
+      metaTagDescriptionContent = metaLinksForDescription.first().attr("content");
+      descriptionList = (List<String>) Arrays.asList(metaTagDescriptionContent.split(" "));
+      metaDescriptionRequired = metaTagDescriptionContent;
+      metaDescriptionFound = true;
+    }
+    else 
+    {
+      metaDescriptionRequired = "NOT_FOUND";
+      metaDescriptionFound = false;
+    }
+    
+
+    Elements metaLinksForKeywords = doc.select("meta[name=keywords]");
+    boolean metaKeywordFound = false;
+    List<String> keyWordsList = new ArrayList<String>();
+  
+    String metaTagKeywordscontent = "", metarKeyWordsRequired = "";
+    
+    if (!metaLinksForKeywords.isEmpty())
+    { 
+      metaTagKeywordscontent = metaLinksForKeywords.first().attr("content");
+      keyWordsList = (List<String>) Arrays.asList(metaTagKeywordscontent.split(","));
+      metarKeyWordsRequired = metaTagKeywordscontent;
+        metaKeywordFound = true;
+    }
+    else 
+    {
+      metarKeyWordsRequired = "NOT_FOUND";
+        metaKeywordFound = false;
+    }
+    
+    HashMap<String, Integer> keywordsHM = new HashMap<String, Integer>();
+    
+    if(metaDescriptionFound)
+    {
+      for(String keyWord : descriptionList)
+      {
+        int count = 1;
+        keyWord = keyWord.replace(",", "");
+        keyWord = keyWord.replace(".", "");
+        keyWord = keyWord.replace(":", "");
+        keyWord = keyWord.replace("*", "");
+        keyWord = keyWord.toLowerCase();
+        boolean frequentWord = checkIfFrequentWord(frequentWords,keyWord);
+        if(!frequentWord)
+        {
+          if(keywordsHM.get(keyWord)==null)
+          {
+            keywordsHM.put(keyWord,count);
+          }
+          else
+          {
+            int cnt = keywordsHM.get(keyWord);
+            keywordsHM.put(keyWord, ++cnt);
+          }
+        }
+        
+      }
+    }
+    
+    if(metaKeywordFound)
+    {
+      for(String keyWord : keyWordsList)
+      {
+        int count = 1;
+        keyWord = keyWord.trim();
+        keyWord = keyWord.toLowerCase();
+        boolean frequentWord = checkIfFrequentWord(frequentWords,keyWord);
+        if(!frequentWord)
+        {
+          if(keywordsHM.get(keyWord)==null)
+          {
+            keywordsHM.put(keyWord,count);
+          }
+          else
+          {
+            int cnt = keywordsHM.get(keyWord);
+            keywordsHM.put(keyWord, ++cnt);
+          }
+        }
+          
+      }
+    }
+    
+    for(String kw: keywordsHM.keySet())
+    {
+      if(titleFound)
+      {
+        if(titleRequired.toLowerCase().contains(kw))
+        {
+          int value = keywordsHM.get(kw);
+          keywordsHM.put(kw, value+2);
+        }
+      }
+      if(queryString.contains(kw))
+      {
+        int value = keywordsHM.get(kw);
+        keywordsHM.put(kw, value+4);
+      }
+    }
+    
+    /*for(String item: keywordsHM.keySet())
+    {
+      int value = keywordsHM.get(item);
+      System.out.println("Key: "+item+"   Value: "+value);
+    }*/
+    
+    int maxValueInMap=(Collections.max(keywordsHM.values()));  // This will return max value in the Hashmap
+        for (Entry<String, Integer> entry : keywordsHM.entrySet()) {  
+            if (entry.getValue()==maxValueInMap) {
+                //System.out.println("The highest occurred word: "+ entry.getKey());     // ---- Print the key with max value
+                jsonResponse+="\""+entry.getKey()+"\"}";
+          response.getWriter().append(jsonResponse);
+          response.getWriter().flush();
+          return;
+            }
+        }
+    }
+    
+
+  public static void main(String[] args) throws Exception {
+    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    server.setHandler(context);
+    context.addServlet(new ServletHolder(new Main()),"/*");
+    server.start();
+    server.join();
+  }
+}
