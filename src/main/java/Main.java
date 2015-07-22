1947dd8cb467be994561705bd0580e5f554fea72
Chandana Srinivasa <csriniva@usc.edu>
2015-07-22 11:25:28 -0700

    Base URI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -82,7 +82,7 @@ public class Main extends HttpServlet {
             //con.userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0");
             con.userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US;   rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"); //for Desktop site
             Document doc = con.get();
-            logger.info("====original body=====" + doc.baseUri());
+            logger.info("~~~~~~~~~~~~~~base URI~~~~~~~~~~~~~~~~~~" + doc.baseUri());
 
             if (url.lastIndexOf('/') <= 7)  // If the URL is like http://twitter.com or http://wikipedia.com then return just THE TITLE
             {
