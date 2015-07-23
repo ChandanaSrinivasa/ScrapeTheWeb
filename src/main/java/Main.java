fd7af51b0445e368814754f945fe6dae99e8592e
Chandana Srinivasa <csriniva@usc.edu>
2015-07-23 12:02:37 -0700

    First recipe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -362,8 +362,11 @@ public class Main extends HttpServlet {
                 else
                 {
                     Elements a = doc.select("a.h-link");
-                    String recipes = a.first().text();
-                    finalQuery = "recipes for " + recipes.trim();
+                    Element firstRecipe = a.first();
+                    if(firstRecipe != null) {
+                        String recipes = firstRecipe.text();
+                        finalQuery = "recipes for " + recipes.trim();
+                    }
 
                 }
 
